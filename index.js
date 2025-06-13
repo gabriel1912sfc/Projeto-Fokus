bdf1186ca265addb4877928ba6b32a5050a228e2
Gabriel Santos <gfonseca882@gmail.com>
2025-06-13 12:27:38 -0300

    Terceiro Commit: adição de um condicional no forEach, para evitar que botões sem data-context interfiram na lógica

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//seleciona os botões e altera o data-contexto a partir do clique
 const html = document.querySelector('html');
 const allBtns = document.querySelectorAll('button');
 
 allBtns.forEach(btn => btn.addEventListener('click', () => {
   let buttonDataContext = btn.getAttribute('data-contexto');
-  console.log(buttonDataContext)
-  html.setAttribute('data-contexto', buttonDataContext); 
+  //Evita que botões sem data-context sejam chamados
+  if (buttonDataContext != null){
+      console.log(buttonDataContext)
+      html.setAttribute('data-contexto', buttonDataContext); 
+  }
 }))
