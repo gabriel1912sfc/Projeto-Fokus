60db1aacb1b3578434677e9341f20ca54daa9bfb
Gabriel Santos <gfonseca882@gmail.com>
2025-06-16 13:07:33 -0300

    Refatoração do evento click com a criação da função 'changeContext'

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 //seleciona os botões e altera o data-contexto a partir do clique
 const html = document.querySelector('html');
 const allBtns = document.querySelectorAll('button');
+const image = document.querySelector('.app__image');
+const appTitle = document.querySelector('.app__title');
+const strongText = document.getElementsByClassName('app__title-strong');
+
 
 allBtns.forEach(btn => btn.addEventListener('click', () => {
   let buttonDataContext = btn.getAttribute('data-contexto');
-  //Evita que botões sem data-context sejam chamados
-  if (buttonDataContext != null){
+  changeContext(buttonDataContext);
+}))
+
+function changeContext(buttonDataContext){
+ //Evita que botões sem data-context sejam chamados.
+    if (buttonDataContext != null){
       console.log(buttonDataContext)
       html.setAttribute('data-contexto', buttonDataContext); 
+      //Troca a imagem de acordo com o contexto do botão clicado. 
+      image.setAttribute('src', `/imagens/${buttonDataContext}.png`);
   }
-}))
+
+  if(buttonDataContext == 'foco'){
+    appTitle.innerHTML = `Otimize sua produtividade,<br>
+                 <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+  } else if(buttonDataContext == 'short'){
+       appTitle.innerHTML = `Que tal dar uma respirada?<br>
+                 <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+  } else if(buttonDataContext == 'long'){
+       appTitle.innerHTML = `Hora de voltar à superfície.<br>
+                 <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+  }
+}
+
